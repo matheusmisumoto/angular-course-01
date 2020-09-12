ce3502ae8a4e407098932f080d6a61479f509669
matheusmisumoto <matheus.japan@gmail.com>
2020-09-11 22:44:41 -0300

    Using inputs and arrays, getting parameters from routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HomeComponent } from './home/home.component';
 import { RandomComponent } from './random/random.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { AboutComponent } from '../about/about.component';
+import { DiscListComponent } from './disc-list/disc-list.component';
+import { DiscDetailsComponent } from './disc-details/disc-details.component';
+import { MultiplicationTableComponent } from './multiplication-table/multiplication-table.component';
 
 // RouterModule.forRoot: módulo que importa os componentes como seções do site navegáveis
 // Array formada por: 
@@ -23,11 +26,13 @@ import { AboutComponent } from '../about/about.component';
       { path: '', component: HomeComponent },
       { path: 'about', component: AboutComponent },
       { path: 'greeting', component: GreetingComponent },
-      { path: 'random', component: RandomComponent }
-
+      { path: 'random', component: RandomComponent },
+      { path: 'multiplication-table', component: MultiplicationTableComponent },
+      { path: 'discs', component: DiscListComponent },
+      { path: 'discs/:index', component: DiscDetailsComponent }
     ])
   ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, RandomComponent, GreetingComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, RandomComponent, GreetingComponent, DiscListComponent, DiscDetailsComponent, MultiplicationTableComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/discs.ts
@@ -0,0 +1,24 @@
+// Array com discos e seus detalhes
+
+export const discs = [
+  {
+    title: "Use your Illusion I",
+    author: "Guns 'n Roses",
+    year: 1992
+  },
+  {
+    title: "Use your Illusion II",
+    author: "Guns 'n Roses",
+    year: 1993
+  },
+  {
+    title: "No Border",
+    author: "JAM Project",
+    year: 2018
+  },
+  {
+    title: "Thriller",
+    author: "Michael Jackson",
+    year: 1987
+  }
+]
